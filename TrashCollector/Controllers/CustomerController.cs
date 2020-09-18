e1732f424f42c9c22f6b6bbed2aa760667452a3d
KyWilkinson <wilki018@hotmail.com>
2020-09-18 15:21:04 -0500

    Authorized the customer controller

--- TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/Controllers/CustomerController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
         // GET: CustomerController
